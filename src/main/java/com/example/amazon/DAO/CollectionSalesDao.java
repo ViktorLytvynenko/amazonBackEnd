40ff78614c6cd3a216a525c07976c8e72f347e4b
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-06-23 08:38:07 +0300

    Connection fix

--- src/main/java/com/example/amazon/DAO/CollectionSalesDao.java
+++ src/main/java/com/example/amazon/DAO/CollectionSalesDao.java
@@ -3,4 +3,4 @@ package com.example.amazon.DAO;
 import com.example.amazon.Data;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface CollectionSalesDao extends MongoRepository<Data, String> { }
+public interface CollectionSalesDao extends MongoRepository<Data, String> {}
